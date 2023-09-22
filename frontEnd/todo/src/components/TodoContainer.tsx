3188508057f3fd5a83ac3c4bd8b19b4c0eea05a4
vanno23 <118561252+vanno23@users.noreply.github.com>
2023-09-22 14:40:16 +0400

    main

--- frontEnd/todo/src/components/TodoContainer.tsx
+++ frontEnd/todo/src/components/TodoContainer.tsx
@@ -1,21 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { GetTodoData } from '../TypeScript/getDataTypes';
-import TodoItem from './TodoItem';
-import { TodoContainerProps } from '../TypeScript/TodoContainerProps';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { GetTodoData } from "../TypeScript/getDataTypes";
+import TodoItem from "./TodoItem";
+import { TodoContainerProps } from "../TypeScript/TodoContainerProps";
 
-const TodoContainer = ({dataIsAdded, setDataIsAdded, setEditData, setInputValue, setDataId, setCompleted}: TodoContainerProps) => {
+const TodoContainer = ({
+  dataIsAdded,
+  setDataIsAdded,
+  setEditData,
+  setInputValue,
+  setDataId,
+  setCompleted,
+}: TodoContainerProps) => {
   const [getTodoData, setGetTodoData] = useState<GetTodoData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/todoApp/getTodoData', {
-          headers: { 'Cache-Control': 'no-cache' }, // Disable caching for this request
-        });
+        const response = await axios.get(
+          "https://mern-todo-app-jwow.onrender.com/todoApp/getTodoData",
+          {
+            headers: { "Cache-Control": "no-cache" }, // Disable caching for this request
+          }
+        );
         setGetTodoData(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
     fetchData();
@@ -23,52 +33,64 @@ const TodoContainer = ({dataIsAdded, setDataIsAdded, setEditData, setInputValue,
 
   const deleteTodoRow = async (id: string) => {
     try {
-      await axios.delete(`http://localhost:5000/todoApp/deleteTodoData/${id}`, {
-        headers: { 'Cache-Control': 'no-cache' }, // Disable caching for this request
-      });
+      await axios.delete(
+        `https://mern-todo-app-jwow.onrender.com/todoApp/deleteTodoData/${id}`,
+        {
+          headers: { "Cache-Control": "no-cache" }, // Disable caching for this request
+        }
+      );
       setDataIsAdded(!dataIsAdded);
     } catch (error) {
-      console.error('Error delete data:', error);
+      console.error("Error delete data:", error);
     }
-  }
+  };
 
   const editFunction = (id: string, title: string, completed: boolean) => {
     setEditData(true);
     setInputValue(title);
     setDataId(id);
     setCompleted(completed);
-  }
+  };
 
   const completeRow = async (id: string, completed: boolean) => {
     try {
-      await axios.put('http://localhost:5000/todoApp/completeRow', {_id: id, completed: !completed}, {
-        headers: { 'Cache-Control': 'no-cache' },
-      },);
+      await axios.put(
+        "https://mern-todo-app-jwow.onrender.com/todoApp/completeRow",
+        { _id: id, completed: !completed },
+        {
+          headers: { "Cache-Control": "no-cache" },
+        }
+      );
       setDataIsAdded(!dataIsAdded);
     } catch (error) {
-      console.error('Error complete row data:', error);
+      console.error("Error complete row data:", error);
     }
-  }
+  };
 
   return (
-    <div className='todoItemContainer'>
-      <div className='todoItemContainerHeader'>
+    <div className="todoItemContainer">
+      <div className="todoItemContainerHeader">
         <div>
           <span>Total Tasks: </span>
-          <span className='totalTasks'>{getTodoData.length}</span>
+          <span className="totalTasks">{getTodoData.length}</span>
         </div>
         <div>
           <span>Completed Tasks: </span>
-          <span className='totalTasks'>1</span>
+          <span className="totalTasks">1</span>
         </div>
       </div>
-      {
-        getTodoData?.map((item, index) => {
-          return (
-            <TodoItem item={item} index={index} key={item._id} deleteTodoRow={deleteTodoRow} editFunction={editFunction} completeRow={completeRow}/>
-          )
-        })
-      }
+      {getTodoData?.map((item, index) => {
+        return (
+          <TodoItem
+            item={item}
+            index={index}
+            key={item._id}
+            deleteTodoRow={deleteTodoRow}
+            editFunction={editFunction}
+            completeRow={completeRow}
+          />
+        );
+      })}
     </div>
   );
 };
--- frontEnd/todo/src/components/TodoInput.tsx
+++ frontEnd/todo/src/components/TodoInput.tsx
@@ -1,61 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
-import { TodoInputProps } from '../TypeScript/TodoInputProps';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { TodoInputProps } from "../TypeScript/TodoInputProps";
 
-const TodoInput = ({dataIsAdded, setDataIsAdded, editData, setEditData, inputValue, setInputValue, dataId, completed, setCompleted}: TodoInputProps) => {
-
-
+const TodoInput = ({
+  dataIsAdded,
+  setDataIsAdded,
+  editData,
+  setEditData,
+  inputValue,
+  setInputValue,
+  dataId,
+  completed,
+  setCompleted,
+}: TodoInputProps) => {
   const changeData = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
       if (!editData) {
-        await axios.post('http://localhost:5000/todoApp', { title: inputValue, completed }, {
-          headers: { 'Cache-Control': 'no-cache' },
-        });
+        await axios.post(
+          "https://mern-todo-app-jwow.onrender.com/todoApp",
+          { title: inputValue, completed },
+          {
+            headers: { "Cache-Control": "no-cache" },
+          }
+        );
       } else {
-        await axios.put('http://localhost:5000/todoApp/changeTodoData', { id: dataId, editedTitle: inputValue, completed }, {
-          headers: { 'Cache-Control': 'no-cache' },
-        });
+        await axios.put(
+          "https://mern-todo-app-jwow.onrender.com/todoApp/changeTodoData",
+          { id: dataId, editedTitle: inputValue, completed },
+          {
+            headers: { "Cache-Control": "no-cache" },
+          }
+        );
         setEditData(false);
       }
       setDataIsAdded(!dataIsAdded);
       setCompleted(false);
-      setInputValue('');
+      setInputValue("");
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
-  
+
   return (
-    <div className='TodoInput'>
-      <h1 className='todoTitle'>Task manager</h1>
+    <div className="TodoInput">
+      <h1 className="todoTitle">Task manager</h1>
 
       <form onSubmit={changeData}>
-        <div className='inputData'>
+        <div className="inputData">
           <input
             type="text"
-            placeholder='Add a task'
+            placeholder="Add a task"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
           />
-          <div className='TodoInputBtns'>
-            <button 
-              className='submitButton' 
-              type='submit'>
-              {editData ? 'Edit' : 'Add'}
+          <div className="TodoInputBtns">
+            <button className="submitButton" type="submit">
+              {editData ? "Edit" : "Add"}
             </button>
-            <button 
-              className={`completeButton ${completed ? 'completeTrue' : ''}`} 
-              type='button' onClick={() => setCompleted(!completed)} 
+            <button
+              className={`completeButton ${completed ? "completeTrue" : ""}`}
+              type="button"
+              onClick={() => setCompleted(!completed)}
               aria-label="Toggle completion"
             >
-              <div className={`${completed ? 'complete' : ''}`}></div>
+              <div className={`${completed ? "complete" : ""}`}></div>
             </button>
           </div>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default TodoInput
+export default TodoInput;
